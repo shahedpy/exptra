cfd6862d76263f07929c1566f75c4486650677b7
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2026-02-20 10:57:09 +0600

    texual change

--- lib/modules/category/category_page.dart
+++ lib/modules/category/category_page.dart
@@ -32,7 +32,7 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Manage Categories'), elevation: 0),
+      appBar: AppBar(title: const Text('Categories'), elevation: 0),
       body: Obx(
         () => controller.isLoading.value
             ? const Center(child: CircularProgressIndicator())
